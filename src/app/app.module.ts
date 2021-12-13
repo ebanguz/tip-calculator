d6fefbe0a5ad64e81cebb617f2b2287eb9ee79de
Esteban Guzmán <57820858+ebanguz@users.noreply.github.com>
2021-12-12 20:34:35 -0400

    git init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CalculatorComponent } from './components/calculator/calculator.component';
+import { DisplayResultsComponent } from './components/display-results/display-results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CalculatorComponent,
+    DisplayResultsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/display-results/display-results.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-results',
+  templateUrl: './display-results.component.html',
+  styleUrls: ['./display-results.component.scss']
+})
+export class DisplayResultsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
